2ff5fed7449d9643455a50cc7a7cc4dcbde5170c
Tomas Rodriguez <tomirodriguez1368@gmail.com>
2022-01-11 14:07:16 -0300

    font awesome rollup

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,11 +1,11 @@
 import React, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
-import { FontAwesomeIcon } from "@fontawesome/react-fontawesome";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
   faMapMarkedAlt,
   faSearch,
   faShoppingCart,
-} from "@fontawesome/free-solid-svg-icons";
+} from "@fortawesome/free-solid-svg-icons";
 import { useDispatch, useSelector } from "react-redux";
 import { logOut } from "../../redux/userRedux";
 import "./NavBar.scss";
