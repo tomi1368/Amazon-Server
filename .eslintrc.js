147eff6fee9b37bde5be498fd540711c6b038104
Tomas Rodriguez <tomirodriguez1368@gmail.com>
2022-03-08 18:15:45 -0300

    fix config eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,23 +1,51 @@
-export default {
+module.exports = {
     "env": {
-        "browser": true,
-        "es2021": true
-    },
+      "browser": true,
+      "es6": true,
+      "jest/globals": true,
+      "node": true
+    },  
     "extends": [
-        "eslint:recommended",
-        "plugin:react/recommended"
+      "eslint:recommended",
+      "plugin:react/recommended"
     ],
     "parserOptions": {
-        "ecmaFeatures": {
-            "jsx": true
-        },
-        "ecmaVersion": "latest",
-        "sourceType": "module"
+      "ecmaFeatures": {
+        "jsx": true
+      },
+      "ecmaVersion": 2018,
+      "sourceType": "module"
     },
     "plugins": [
-        "react",
-        "cypress"
+      "react", "jest","cypress"
     ],
     "rules": {
+      "indent": [
+        "error",
+          2
+        ],
+      "linebreak-style": [
+        "error",
+        "unix"
+      ],
+      "quotes": [
+        "error",
+        "single"
+      ],
+      "semi": [
+        "error",
+        "never"
+      ],
+      "eqeqeq": "error",
+      "no-trailing-spaces": "error",
+      "object-curly-spacing": [
+        "error", "always"
+      ],
+      "arrow-spacing": [
+        "error", { "before": true, "after": true }
+      ],
+      "no-console": "warn",
+      "react/prop-types": 0
     }
-}
+  }
+  
